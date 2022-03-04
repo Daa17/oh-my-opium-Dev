bafd7931c7c64d94a9bcc83c615408978acd3191
David Arzumanyan <devarzumanyan@gmail.com>
2022-03-04 16:39:09 +0400

    adding mui dropDown checkbox radio

--- src/Components/Filters/index.tsx
+++ src/Components/Filters/index.tsx
@@ -1,88 +1,83 @@
-import {DropdownSelector, Tabs, Button } from '@opiumteam/react-opium-components'
-import React, { FC } from 'react'
+import React, { FC } from "react";
+import { Tabs, Button } from "@opiumteam/react-opium-components";
+import MuiDropDown from "../DropDown";
 // import PoolsList from '../PoolsList'
 
-import '../../styles/main.scss'
-import './styles.scss'
+import "../../styles/main.scss";
+import "./styles.scss";
 
 const tabItems = [
-    {title: "All pools", eventKey: "All pools", content: <span></span>},
-    {title: "My stake", eventKey: "My stake", content: <span></span>}
-    
-]
+  { title: "All pools", eventKey: "All pools", content: <span></span> },
+  { title: "My stake", eventKey: "My stake", content: <span></span> },
+];
 const programsDropdownItems = [
-    { title: 'turbo', value: 'turbo'},
-    { title: 'inshurance', value: 'inshurance' },
-    { title: '$OPIUM products', value: '$OPIUM products'},
-]
+  { title: "turbo", value: "turbo" },
+  { title: "inshurance", value: "inshurance" },
+  { title: "$OPIUM products", value: "$OPIUM products" },
+];
 const sortDropdownItems = [
-    { title: 'expiration date', value: 'expiration date'},
-    { title: 'liquidity', value: 'liquidity' },
-    { title: 'APR', value: 'APR'},
-    { title: 'name', value: 'name'},
-]
+  { title: "expiration date", value: "expiration date" },
+  { title: "liquidity", value: "liquidity" },
+  { title: "APR", value: "APR" },
+  { title: "name", value: "name" },
+];
 const mobileFilterItems = [
-    { title: 'turbo', value: 'turbo'},
-    { title: 'inshurance', value: 'inshurance' },
-    { title: '$OPIUM products', value: '$OPIUM products'},
-    { title: 'expiration date', value: 'expiration date'},
-    { title: 'liquidity', value: 'liquidity' },
-    { title: 'APR', value: 'APR'},
-    { title: 'name', value: 'name'},
-]
-const applyFilter = () => {
-
-}
+  { title: "turbo", value: "turbo" },
+  { title: "inshurance", value: "inshurance" },
+  { title: "$OPIUM products", value: "$OPIUM products" },
+  { title: "expiration date", value: "expiration date" },
+  { title: "liquidity", value: "liquidity" },
+  { title: "APR", value: "APR" },
+  { title: "name", value: "name" },
+];
+const applyFilter = () => {};
 const Filters: FC<{}> = () => {
-    return (
-        <div className="filters_wrapper">
-            <div className="filters_tab_wrapper">
-                <Tabs
-                    id= "filters"
-                    items={tabItems}
-                    // defaultActiveKey="pools"
-                />
-            </div>
-            <div className="dropdowns_container">
-                <div className="dropdown-wrapper">
-                    <DropdownSelector
-                        title="Programs"
-                        items={programsDropdownItems}
-                    />
-                </div>
-                <div className="sort_dropdown">
-                    <span>Sort by:</span>
-                    <div className="dropdown-wrapper">
-                        <DropdownSelector
-                                title="expiration date"
-                                items={sortDropdownItems}
-                        />
-                    </div>
-                </div>
-            </div>
-            <div className="mobile_dropdowns">
-                <div className="dropdown-wrapper">
-                    <DropdownSelector
-                           title=" "
-                           items={mobileFilterItems}
-                           className="filter_dropdown"
-                           
-                    >
-                    <Button
-                        variant='secondary'
-                        className="apply_filter"
-                        style={{
-                            backgroundColor: 'transparent',
-                            color: "#fff"
-                        }}
-                        label="apply"
-                        onClick={applyFilter}
-                    />
-                    </DropdownSelector>
-                </div>
-            </div>
+  return (
+    <div className="filters_wrapper">
+      <div className="filters_tab_wrapper">
+        <Tabs
+          id="filters"
+          items={tabItems}
+          // defaultActiveKey="pools"
+        />
+      </div>
+      <div className="dropdowns_container">
+        <div className="dropdown-wrapper">
+          <MuiDropDown
+            isCheckbox
+            title="Programs"
+            data={programsDropdownItems}
+          />
         </div>
-    )
-}
+        <div className="sort_dropdown">
+          <span>Sort by:</span>
+          <div className="dropdown-wrapper">
+            <MuiDropDown title="expiration date" data={sortDropdownItems} />
+          </div>
+        </div>
+      </div>
+      <div className="mobile_dropdowns">
+        <div className="dropdown-wrapper">
+          {/* <MuiDropDown
+            title=" "
+            items={mobileFilterItems}
+            className="filter_dropdown"
+          > */}
+          <Button
+            variant="secondary"
+            className="apply_filter"
+            style={{
+              backgroundColor: "transparent",
+              color: "#fff",
+            }}
+            label="apply"
+            onClick={applyFilter}
+          />
+          {/* </MuiDropDown> */}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Filters
+export default Filters;
